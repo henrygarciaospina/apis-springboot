c3ef9c5622b280017cbd33a1c20f2b67a7ae0711
HENRY GARCIA OSPINA <henrygarcia0309@gmail.com>
2022-04-30 10:50:16 -0500

    modificaciòn clientes y properties

--- mongo-springboot/src/main/java/com/prestamos/mongospringboot/models/Cliente.java
+++ mongo-springboot/src/main/java/com/prestamos/mongospringboot/models/Cliente.java
@@ -1,16 +1,17 @@
 package com.prestamos.mongospringboot.models;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Document(collection = "clientes")
 public class Cliente {
     @Id
     private Long id;
